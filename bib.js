32085733653041e5e6be57699f572a6610b3561a
David Karger <karger@mit.edu>
2020-02-23 17:40:25 -0500

    Updated bib.js

--- bib.js
+++ bib.js
@@ -2,20 +2,24 @@
 	"reference": [
 		{
 			"link": "http://consulproject.org/en/",
-			"title": "Consul Project"
+			"title": "Consul Project",
+			"category": "Forums"
 		},
 		{
 			"link": "https://www.publicknowledge.org/blog/moderating-race-on-platforms",
-			"title": "Moderating Race on Platforms"
+			"title": "Moderating Race on Platforms",
+			"category": "Forums"
 		},
 		{
 			"link": "https://www.mightynetworks.com/facebook-group-alternative",
 			"title": "Might Networks",
-			"notes": "Proposed alternative to Facebook, though it looks more like Google Groups"
+			"notes": "Proposed alternative to Facebook, though it looks more like Google Groups",
+			"category": "Forums"
 		},
 		{
 			"link": "https://www.newyorker.com/culture/annals-of-inquiry/the-case-for-open-borders",
-			"title": "The Case for Open Borders (Review)"
+			"title": "The Case for Open Borders (Review)",
+			"category": "Forums"
 		}
 	],
 	"category": [
