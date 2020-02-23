808e7d563faba51a512d1d488ce3b923531e9d33
David Karger <karger@mit.edu>
2020-02-23 16:54:42 -0500

    Updated bib.js

--- bib.js
+++ bib.js
@@ -9,6 +9,12 @@
 			"link": "https://www.publicknowledge.org/blog/moderating-race-on-platforms",
 			"title": "Moderating Race on Platforms",
 			"category": "Forums"
+		},
+		{
+			"link": "https://www.mightynetworks.com/facebook-group-alternative",
+			"title": "Might Networks",
+			"notes": "Proposed alternative to Facebook, though it looks more like Google Groups",
+			"category": "Forums"
 		}
 	],
 	"category": [
