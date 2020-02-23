e029332cd23b5bcf91a7dee3fba5e2603d266876
David Karger <karger@mit.edu>
2020-02-23 16:53:32 -0500

    Updated bib.js

--- bib.js
+++ bib.js
@@ -4,6 +4,11 @@
 			"link": "http://consulproject.org/en/",
 			"title": "Consul Project",
 			"category": "Forums"
+		},
+		{
+			"link": "https://www.publicknowledge.org/blog/moderating-race-on-platforms",
+			"title": "Moderating Race on Platforms",
+			"category": "Forums"
 		}
 	],
 	"category": [
