283ef8daa07f31de3aef7549316edfdc0ea53426
David Karger <karger@mit.edu>
2020-02-23 17:21:26 -0500

    Updated bib.js

--- bib.js
+++ bib.js
@@ -15,10 +15,16 @@
 			"title": "Might Networks",
 			"notes": "Proposed alternative to Facebook, though it looks more like Google Groups",
 			"category": "Forums"
+		},
+		{
+			"link": "https://www.newyorker.com/culture/annals-of-inquiry/the-case-for-open-borders",
+			"title": "The Case for Open Borders (Review)",
+			"category": "Forums"
 		}
 	],
 	"category": [
 		"Forums",
-		"Tech"
+		"Tech",
+		"Politics"
 	]
 }
